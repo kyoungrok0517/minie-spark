a3389425e4f0f56d07f9c26ad3d17401d508a99c
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2019-11-04 19:09:52 +0900

    match exo pipeline

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -25,11 +25,12 @@ object Main {
     val data_path = args(0)
     val out_dir = args(1)
     val n_partitions = args(2).toInt
-    //    val n_workers = args(3).toInt
-    val now = Calendar.getInstance().getTime()
-    val formatter = new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss")
-    val timestamp = formatter.format(now)
-    val out_path = s"$out_dir/$timestamp"
+    // val n_workers = args(3).toInt
+    // val now = Calendar.getInstance().getTime()
+    // val formatter = new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss")
+    // val timestamp = formatter.format(now)
+    // val out_path = s"$out_dir/$timestamp"
+    val out_path = s"$out_dir"
 
     val spark = SparkSession
       .builder()
@@ -47,64 +48,112 @@ object Main {
 
     val totalCount = sc.broadcast(df.count())
     println("Total count: " + totalCount.value)
+    // println(df.columns.toSeq)
+    // System.exit(1)
 
     // Run
-    val results = rows.repartition(n_partitions).mapPartitions(row => {
-      // Initialize the parser and MinIE// Initialize the parser and MinIE
-      val parser: StanfordCoreNLP = CoreNLPUtils.StanfordDepNNParser()
-      var sg: SemanticGraph = new SemanticGraph()
-      //      val dictionaries = Array[String]("/minie-resources/wiki-freq-rels-mw.txt", "/minie-resources/wiki-freq-args-mw.txt")
-      //      val dict = new Dictionary(dictionaries)
-      val minie: MinIE = new MinIE()
+    val results = rows
+      .repartition(n_partitions)
+      .mapPartitions(row => {
+        // Initialize the parser and MinIE
+        val parser: StanfordCoreNLP = CoreNLPUtils.StanfordDepNNParser()
+        var sg: SemanticGraph = new SemanticGraph()
+        val minie: MinIE = new MinIE()
+        val dictionaries = Array[String](
+          "/minie-resources/wiki-freq-rels-mw.txt",
+          "/minie-resources/wiki-freq-args-mw.txt"
+        )
+        val dict = new Dictionary(dictionaries)
 
-      // Extract
-      val results_ = row.flatMap(r => {
-        val id: String = r(0).toString
-        val sentence: String = r(1).toString
-        val sid: String = r(2).toString
+        // Extract
+        val results_ = row.flatMap(r => {
+          // (file, collection, p_id, s_id, sentence, index)
+          // val id: String = r.lastOption
+          val file: String = r(0).toString
+          val collection: String = r(1).toString
+          val p_id: String = r(2).toString
+          val s_id: String = r(3).toString
+          val sentence: String = r(4).toString
 
-        // process data
-        try {
-          sg = CoreNLPUtils.parse(parser, sentence)
-          minie.minimize(sentence, sg, MinIE.Mode.SAFE, null)
-          //          minie.minimize(sentence, sg, MinIE.Mode.DICTIONARY, dict)
-        } catch {
-          case e: Exception =>
-            (id, sentence, sid, "")
-        }
+          // process data
+          try {
+            sg = CoreNLPUtils.parse(parser, sentence)
+            // minie.minimize(sentence, sg, MinIE.Mode.SAFE, null)
+            minie.minimize(sentence, sg, MinIE.Mode.DICTIONARY, dict)
+          } catch {
+            case e: Exception =>
+              (sentence, s_id, "")
+          }
 
-        // Do stuff with the triples// Do stuff with the triples
-        val props: ObjectArrayList[AnnotatedProposition] = minie.getPropositions.clone()
+          // Do stuff with the triples
+          val props: ObjectArrayList[AnnotatedProposition] =
+            minie.getPropositions.clone()
 
-        // Clear the object for re-usability
-        minie.clear
+          // Clear the object for re-usability
+          minie.clear
 
-        // return
-        props.elements().map(prop => {
-          // triple
-          val subj = Option(prop.getSubject).getOrElse("")
-          val rel = Option(prop.getRelation).getOrElse("")
-          val obj = Option(prop.getObject).getOrElse("")
+          // return
+          props
+            .elements()
+            .map(prop => {
+              // triple
+              val subj = Option(prop.getSubject).getOrElse("")
+              val rel = Option(prop.getRelation).getOrElse("")
+              val obj = Option(prop.getObject).getOrElse("")
 
-          // annotations
-          val polarity = Option(prop.getPolarity.getType).getOrElse("")
-          val modality = Option(prop.getModality.getModalityType).getOrElse("")
-          val quantity = prop.getAllQuantities.elements()
-            .filter(q => {
-              !Option(q).isEmpty
-            }).map(q => q.toString).mkString("|")
+              // annotations
+              val polarity = Option(prop.getPolarity.getType).getOrElse("")
+              val modality =
+                Option(prop.getModality.getModalityType).getOrElse("")
+              val quantity = prop.getAllQuantities
+                .elements()
+                .filter(q => {
+                  !Option(q).isEmpty
+                })
+                .map(q => q.toString)
+                .mkString("|")
 
-          val triple: String = s"$subj\t$rel\t$obj\t$polarity\t$modality\t$quantity"
-          (id, sentence, sid, triple)
+              val triple: String =
+                s"$subj\t$rel\t$obj\t$polarity\t$modality\t$quantity"
+              (
+                file,
+                collection,
+                p_id,
+                s_id,
+                sentence,
+                subj,
+                rel,
+                obj,
+                polarity,
+                modality,
+                quantity
+              )
+            })
+
         })
 
+        // return
+        results_
       })
 
-      // return
-      results_
-    })
-
     // Save
-    results.toDF("id", "line", "sid", "triple").write.option("compression", "snappy").parquet(out_path)
+    results
+      .toDF(
+        "file",
+        "collection",
+        "p_id",
+        "s_id",
+        "sentence",
+        "triple_subj",
+        "triple_rel",
+        "triple_obj",
+        "triple_polarity",
+        "triple_modality",
+        "triple_quantity"
+      )
+      .write
+      .option("compression", "gzip")
+      .parquet(out_path)
+    sc.stop()
   }
 }
